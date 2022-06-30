ae2226efa3bff837a1839494bb8d6b37727e711d
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-06-30 11:00:29 +0800

    v-for

---
+++ src/05v-for.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <ul>
+        <li v-for="item in arr" :key="item">
+          {{ item }}
+        </li>
+        <li v-for="(item, index) in arr" :key="index">
+          {{ index }} - {{ item }}
+        </li>
+      </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: ["刘昊然", "易烊千玺", "肖战","任嘉伦"],
+    };
+  },
+};
+</script>
