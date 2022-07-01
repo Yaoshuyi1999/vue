9f17534775414ee73fa8a81f66b8341dd0987ed2
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-01 17:56:01 +0800

    基本vue结束

---
+++ src/09附加练习-选你爱我求和.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="(item,index) in arr" :key="index">
+      <input type="checkbox" v-model="isAll" :value="item">
+      <span>{{item}}</span>
+    </div>
+    <p>你选中的元素，累加的值为：{{num}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      arr:[9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      isAll:[]
+    }
+  },
+  computed:{
+    num(){
+      return this.isAll.reduce((sum,val)=>{return (sum += val)},0)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/品牌管理.vue
+++ src/品牌管理.vue
@@ -1,6 +1,5 @@
-// yarn add bootstrap
-// yarn add moment
-// import "bootstrap/dist/css/bootstrap.css"   main.js中
+// yarn add bootstrap // yarn add moment // import
+"bootstrap/dist/css/bootstrap.css" main.js中
 <template>
   <div id="app">
     <div class="container">
@@ -24,21 +23,21 @@
         </thead>
         <tbody>
           <tr v-for="obj in list" :key="obj.id">
-            <td>{{obj.id}}</td>
-            <td>{{obj.name}}</td>
+            <td>{{ obj.id }}</td>
+            <td>{{ obj.name }}</td>
 
             <!-- 如果价格超过100，就有red这个类 -->
-            <td :class="{red:obj.price>100}">{{obj.price}}</td>
-            <td>{{obj.time | format}}</td>
+            <td :class="{ red: obj.price > 100 }">{{ obj.price }}</td>
+            <td>{{ obj.time | format }}</td>
             <td><a href="#" @click.prevent="delFn(obj.id)">删除</a></td>
           </tr>
-          <tr style="background-color: #EEE">
-              <td>统计:</td>
-              <td colspan="2">总价钱为: {{allPrice}}</td>
-              <td colspan="2">平均价: {{averagePrice}}</td>
+          <tr style="background-color: #eee">
+            <td>统计:</td>
+            <td colspan="2">总价钱为: {{ allPrice }}</td>
+            <td colspan="2">平均价: {{ averagePrice }}</td>
           </tr>
         </tbody>
-          <!-- 
+        <!-- 
         <tfoot >
           <tr>
             <td colspan="5" style="text-align: center">暂无数据</td>
@@ -48,7 +47,7 @@
       </table>
 
       <!-- 添加资产 -->
-      <form class="form-inline" style="display:flex;">
+      <form class="form-inline" style="display: flex">
         <div class="form-group">
           <div class="input-group">
             <input
@@ -84,62 +83,72 @@
 // 3. 下载bootstrap, main.js引入bootstrap.css
 // 4. 把list数组 - 铺设表格
 // 5. 修改价格颜色
-import moment from "moment";
+import moment from 'moment';
 export default {
   data() {
     return {
-      list: [
-        { id: 100, name: "外套", price: 199, time: new Date('2010-08-12')},
-        { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
-        { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
-        { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
-      ],
-      name:'',
-      price:0
-    }
+      list: JSON.parse(localStorage.getItem('list')) || [],
+      // list:[
+      //   { id: 100, name: "外套", price: 199, time: new Date('2010-08-12')},
+      //   { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
+      //   { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
+      //   { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
+      // ],
+      name: '',
+      price: 0,
+    };
   },
   methods: {
-    addFn(){
-        if (this.name.trim().length == 0 || this.price == 0) {
-            return alert("请输入完整的信息");
-        }
-      const id=this.list[this.list.length-1]
-      ?this.list[this.list.length-1].id+1
-      :100;
-       this.list.push({
+    addFn() {
+      if (this.name.trim().length == 0 || this.price == 0) {
+        return alert('请输入完整的信息');
+      }
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({
         id,
-        name:this.name,
-        price:this.price,
-        time:new Date()
-       });
-       this.name='';
-       this.price=0;
+        name: this.name,
+        price: this.price,
+        time: new Date(),
+      });
+      this.name = '';
+      this.price = 0;
     },
-    delFn(id){
-        const index=this.list.findIndex((ele)=>{
-            return id==ele.id
-        })
-        this.list.splice(index,1)
-    }
+    delFn(id) {
+      const index = this.list.findIndex((ele) => {
+        return id == ele.id;
+      });
+      this.list.splice(index, 1);
+    },
   },
   filters: {
-    format(val){
+    format(val) {
       return moment(val).format('YYYY-MM-DD');
-    }
+    },
   },
   computed: {
-      allPrice(){
-          return this.list.reduce((sum, next) => sum += next.price, 0).toFixed(2)
+    allPrice() {
+      return this.list.reduce((sum, next) => (sum += next.price), 0).toFixed(2);
+    },
+    averagePrice() {
+      return (this.allPrice / this.list.length).toFixed(2);
+    },
+  },
+  watch: {
+    list: {
+      handler(newVal) {
+        localStorage.setItem('list', JSON.stringify(newVal || []));
       },
-      averagePrice(){
-          return (this.allPrice / this.list.length).toFixed(2)
-      }
-  }
+      deep: true,
+      immediate: true,
+    },
+  },
 };
 </script>
 
-<style >
-.red{
+<style>
+.red {
   color: red;
 }
 </style>
