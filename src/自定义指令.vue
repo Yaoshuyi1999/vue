644f7083804a0aac87d5b988fa335aff637e6f25
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-07 10:23:48 +0800

    自定义指令1

---
+++ src/自定义指令.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <UseDirective></UseDirective>
+  </div>
+</template>
+
+<script>
+import UseDirective from './components/07_UseDirective'
+export default {
+  components: {
+    UseDirective
+  }
+}
+</script>
+
+<style>
+
+</style>
